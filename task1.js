084f86046f9365b59d4be3e12124f4630b58ab1f
Andrew Dorokhin <andrew@dorokhin.moscow>
2018-05-31 18:33:43 +0700

    fix task1 solution

--- task1.js
+++ task1.js
@@ -1,15 +1,14 @@
 function convertBase(value, fromBase, toBase) {
   if (value > 10000 || value < 0) throw new Error('Invalid value');
-  if (fromBase > 36 || fromBase < 1) throw new Error('Invalid base');
-  if (toBase > 36 || toBase < 1) throw new Error('Invalid base');
+  if (fromBase > 36 || fromBase < 2) throw new Error('Invalid base');
+  if (toBase > 36 || toBase < 2) throw new Error('Invalid base');
 
   const range = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
   const fromRange = range.slice(0, fromBase);
   const toRange = range.slice(0, toBase);
   let decValue = value.split('').reverse().reduce((carry, digit, index) => {
     if (fromRange.indexOf(digit) === -1) throw new Error(`Invalid digit \`${digit}\` for base ${fromBase}.`);
-    const result = carry + (fromRange.indexOf(digit) * (fromBase ** index));
-    return result;
+    return carry + (fromRange.indexOf(digit) * (fromBase ** index));
   }, 0);
 
   let newValue = '';
@@ -20,4 +19,4 @@ function convertBase(value, fromBase, toBase) {
   return newValue || '0';
 }
 
-process.stdout.write(convertBase(process.argv[2], process.argv[3], process.argv[4]));
+process.stdout.write(convertBase(process.argv[1], process.argv[2], process.argv[3]));
